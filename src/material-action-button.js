6347d2972aa524dcb1261418a4574112046c6a6d
odaumas <odaumas@amadeus.com>
2017-01-09 10:43:28 +0100

    Pass the index of the clicked element when click called

--- src/material-action-button.js
+++ src/material-action-button.js
@@ -1,10 +1,7 @@
-'use strict';
-
 /**
  * Private method doing the factory
  */
 function actionButtonFactory($rootScope, $compile, $ionicBody, $animate, $ionicTabsDelegate) {
-
   return {
     create: actionButton
   };
@@ -12,129 +9,107 @@ function actionButtonFactory($rootScope, $compile, $ionicBody, $animate, $ionicT
   function actionButton(options) {
     var scope = $rootScope.$new(true);
     var visible = true;
-
     scope.options = options;
     scope.options.removeOnStateChange = true;
     scope.options.bottom = scope.options.hasTab ? '55px' : '15px';
     scope.options.menuBottom = scope.options.hasTab ? '115px' : '90px';
     scope.menuOpened = false;
-
     // Compile the template
     var element = scope.element = $compile(
       '<material-action-button on-create="show()" options="options" dispatcher="buttonClicked(data)"></material-action-button>'
     )(scope);
-
     var mainButtonElement;
     var secondaryButtonsContainer;
-
     //append to the body
     $ionicBody.append(element);
-
     /**
      * Button remove
      */
     scope.removeButton = function() {
-        if (scope.removed) {
-          return;
-        }
-
-        scope.removed = true;
-        return $animate
-          .addClass(mainButtonElement, 'action-button-hide')
-          .then(function() {
-            visible = false;
-            scope.$destroy();
-            element.remove();
-            return;
-          })
+      if (scope.removed) {
+        return;
       }
-      /**
-       * Button clicked
-       */
+      scope.removed = true;
+      return $animate.addClass(mainButtonElement, 'action-button-hide').then(function() {
+        visible = false;
+        scope.$destroy();
+        element.remove();
+        return;
+      });
+    };
+    /**
+     * Button clicked
+     */
     scope.buttonClicked = function(data) {
-        //we handle main button clicks
-        if (data.type === 'main') {
-          //if we have secondary actions...
-          if (data.openMenu) {
-            //if menu is opened, we close it
-            if (scope.menuOpened) {
-              element.removeClass('action-button-menu-show');
-              element.addClass('action-button-menu-hide');
-              $animate.addClass(secondaryButtonsContainer, 'action-button-small-hide')
-                .then(function() {
-                  secondaryButtonsContainer.removeClass('action-button-small-show');
-                  secondaryButtonsContainer.removeClass('action-button-small-hide');
-                  secondaryButtonsContainer.addClass('action-button-small-container__hidden');
-                  scope.menuOpened = false;
-                })
-            } else {
-              //if menu is closed, we open it
-              element.removeClass('action-button-menu-hide');
-              element.addClass('action-button-menu-show');
-
-
-              secondaryButtonsContainer.removeClass('action-button-small-container__hidden');
-              $animate.addClass(secondaryButtonsContainer, 'action-button-small-show')
-                .then(function() {
-                  scope.menuOpened = true;
-                })
-
-            }
+      //we handle main button clicks
+      if (data.type === 'main') {
+        //if we have secondary actions...
+        if (data.openMenu) {
+          //if menu is opened, we close it
+          if (scope.menuOpened) {
+            element.removeClass('action-button-menu-show');
+            element.addClass('action-button-menu-hide');
+            $animate.addClass(secondaryButtonsContainer, 'action-button-small-hide').then(function() {
+              secondaryButtonsContainer.removeClass('action-button-small-show');
+              secondaryButtonsContainer.removeClass('action-button-small-hide');
+              secondaryButtonsContainer.addClass('action-button-small-container__hidden');
+              scope.menuOpened = false;
+            });
           } else {
-            //if we don't have secondary actions
-            if (scope.options.mainAction.onClick) {
-              return scope.options.mainAction.onClick(data.ngEvent);
-            }
-            return;
+            //if menu is closed, we open it
+            element.removeClass('action-button-menu-hide');
+            element.addClass('action-button-menu-show');
+            secondaryButtonsContainer.removeClass('action-button-small-container__hidden');
+            $animate.addClass(secondaryButtonsContainer, 'action-button-small-show').then(function() {
+              scope.menuOpened = true;
+            });
           }
-        }
-
-        if (data.type === 'secondary') {
-          if (scope.options.buttons[data.index].onClick) {
-            return scope.options.buttons[data.index].onClick(data.ngEvent, data.index);
+        } else {
+          //if we don't have secondary actions
+          if (scope.options.mainAction.onClick) {
+            return scope.options.mainAction.onClick(data.ngEvent);
           }
           return;
         }
       }
-      /**
-       * Show the menu
-       */
+      if (data.type === 'secondary') {
+        if (scope.options.buttons[data.index].onClick) {
+          return scope.options.buttons[data.index].onClick(data.ngEvent, data.index);
+        }
+        return;
+      }
+    };
+    /**
+     * Show the menu
+     */
     scope.show = function() {
-
       $animate.addClass(element, 'button-active') //hack to wait till the element is appended? dont know...
         .then(function() {
           visible = true;
           mainButtonElement = angular.element(element[0].getElementsByClassName('action-button'));
           secondaryButtonsContainer = angular.element(element[0].getElementsByClassName(
             'action-button-small-container'));
-
           if (scope.options.startHidden) {
             return;
           }
           return $animate.addClass(mainButtonElement, 'action-button-show');
         });
-    }
-
+    };
     scope.showButton = function() {
-      return $animate.addClass(mainButtonElement, 'action-button-show')
-        .then(function() {
-          visible = true;
-          return;
-        })
-    }
-
+      return $animate.addClass(mainButtonElement, 'action-button-show').then(function() {
+        visible = true;
+        return;
+      });
+    };
     scope.hideButton = function() {
-      return $animate
-        .addClass(mainButtonElement, 'action-button-hide')
-        .then(function() {
-          mainButtonElement.removeClass('action-button-show');
-          mainButtonElement.removeClass('action-button-hide');
-          element.removeClass('action-button-menu-hide');
-          visible = false;
-          return;
-        });
-    }
-
+      return $animate.addClass(mainButtonElement, 'action-button-hide').then(function() {
+        mainButtonElement.removeClass('action-button-show');
+        mainButtonElement.removeClass('action-button-hide');
+        element.removeClass('action-button-menu-hide');
+        visible = false;
+        return;
+      });
+    };
     //auto remove when state changes
     $rootScope.$on('$stateChangeSuccess', function() {
       if (scope.options.removeOnStateChange) {
@@ -142,8 +117,6 @@ function actionButtonFactory($rootScope, $compile, $ionicBody, $animate, $ionicT
         return;
       }
     });
-
-
     return {
       show: function() {
         return scope.showButton();
@@ -154,70 +127,68 @@ function actionButtonFactory($rootScope, $compile, $ionicBody, $animate, $ionicT
       visible: function() {
         return visible;
       }
-    }
+    };
   }
 }
-
 /**
  * Action Button module is initialized here
  */
-var module = angular.module('$actionButton', ['ngAnimate']);
-
-module.factory('$actionButton', ['$rootScope',
-  '$compile', '$ionicBody', '$animate', '$ionicTabsDelegate', actionButtonFactory
-]);
-
-module.component('materialActionButton', {
-  templateUrl: 'template.html',
-  bindings: {
-    options: '<',
-    dispatcher: '&',
-    onCreate: '&'
-  },
-  controller: ['$timeout', function($timeout) {
-    var _this = this;
-    // this.opened = false;
-
-    this.closeMenu = function(data) {
-      //we simulate a main button touch order to close
-      data.openMenu = true;
-      _this.dispatcher({
-        data: data
-      });
-      return;
-    }
-
-    this.$postLink = function() {
-      $timeout(function() {
-        _this.onCreate();
-      })
-    }
-
-    this.buttonClicked = function(data) {
-
-      //if the element has no buttons configured...
-      if (!_this.options.buttons || !_this.options.buttons.length) {
-        _this.dispatcher({
-          data: data
-        });
-        return;
+angular.module('$actionButton', ['ngAnimate'])
+  .factory('$actionButton', [
+    '$rootScope',
+    '$compile',
+    '$ionicBody',
+    '$animate',
+    '$ionicTabsDelegate',
+    actionButtonFactory
+  ])
+  .component('materialActionButton', {
+    templateUrl: 'template.html',
+    bindings: {
+      options: '<',
+      dispatcher: '&',
+      onCreate: '&'
+    },
+    controller: [
+      '$timeout',
+      function($timeout) {
+        var _this = this;
+        // this.opened = false;
+        this.closeMenu = function(data) {
+          //we simulate a main button touch order to close
+          data.openMenu = true;
+          _this.dispatcher({
+            data: data
+          });
+          return;
+        };
+        this.$postLink = function() {
+          $timeout(function() {
+            _this.onCreate();
+          });
+        };
+        this.buttonClicked = function(data) {
+          //if the element has no buttons configured...
+          if (!_this.options.buttons || !_this.options.buttons.length) {
+            _this.dispatcher({
+              data: data
+            });
+            return;
+          }
+          //if the element has buttons configured...
+          //we pass the openMenu true prop...
+          if (data.type === 'main') {
+            data.openMenu = true;
+            _this.dispatcher({
+              data: data
+            });
+            return;
+          }
+          //any other click here has to be from a small button
+          _this.dispatcher({
+            data: data
+          });
+        };
       }
-
-      //if the element has buttons configured...
-      //we pass the openMenu true prop...
-      if (data.type === 'main') {
-        data.openMenu = true;
-        _this.dispatcher({
-          data: data
-        });
-        return;
-      }
-
-      //any other click here has to be from a small button
-      _this.dispatcher({
-        data: data
-      });
-
-    };
-  }]
-});
+    ]
+  });
