9d913197567f7e1969a8c3fab59af38c1be89cfd
odaumas <odaumas@amadeus.com>
2017-01-24 12:26:16 +0100

    Create a publish script

--- publish.sh
+++ publish.sh
@@ -10,10 +10,10 @@ PACKAGE_VERSION=$(cat package.json \
   | tr -d '[[:space:]]')
 
 git add -u;
-git commit -m "Release version $PACKAGE_VERSION" --no-verify
+git commit --no-verify
 git push
-
-git tag -a v$PACKAGE_VERSION -m "Version $PACKAGE_VERSION"
+commitMessage = "$(git log -1 --pretty=%B)"
+git tag -a v$PACKAGE_VERSION -m "Version $PACKAGE_VERSION:$commitMessage"
 git push origin --tags
 
 npm publish
